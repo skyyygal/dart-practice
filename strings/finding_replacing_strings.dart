d81cc3555c3aeb877647f5dd286510fd7e30d7eb
skyyygal <heeramnakp@gmail.com>
2025-11-30 03:31:14 +0530

    FIZZ BUZZ Problem

--- strings/finding_replacing_strings.dart
+++ strings/finding_replacing_strings.dart
@@ -1,25 +1,62 @@
 //finding and replacing the strings.
 
 // void main() {
-//   const text = 'I like pizza';
-//   const toppings = 'with tomatoes';
-//   const favorite = '$text $toppings';
-//   final newText = favorite.replaceAll('pizza', 'pasta');
-//   const newFavorite = 'Now i like curry';
-//   print(newFavorite);
-//   print(newText);
+/*   const text = 'I like pizza';
+  const toppings = 'with tomatoes';
+  const favorite = '$text $toppings';
+  final newText = favorite.replaceAll('pizza', 'pasta');
+  const newFavorite = 'Now i like curry';
+  print(newFavorite);
+  print(newText); */
+// enum Medal { gold, silver, bronze, try_again }
+enum Operation { plus, minus, multiply, divide }
 
 void main() {
-  for (var i = 1; i <= 15; i++) {
-    if (i % 3 == 0 && i % 5 == 5) {
-      print("Fizz buzz");
-    } else if (i % 3 == 0) {
-      print("fizz");
-    } else if (i % 5 == 0) {
-      print('buzz');
-    } else {
-      print(i);
-    }
+  const a = 4;
+  const b = 2;
+  const op = Operation.plus;
+
+  switch (op) {
+    case Operation.plus:
+      int c = a + b;
+      print(c);
+      break;
+    case Operation.minus:
+      int c = a - b;
+      print(c);
+      break;
+
+    case Operation.multiply:
+      int a = 6;
+      int b = 2;
+      int c = a * b;
+      print(c);
+    case Operation.divide:
+      int a = 6;
+      int b = 2;
+      double c = a / b;
+      print(c);
+      break;
   }
-  print("done");
 }
+
+
+/*   const medal = Medal.gold;
+  print(medal);
+  print(medal.name);
+  print(Medal.values);
+  print(Medal.values.byName('silver'));
+  switch (medal) {
+    case Medal.gold:
+      print("First place");
+      break;
+    case Medal.silver:
+      print("Second Place");
+      break;
+    case Medal.bronze:
+      print("Third Place");
+      break;
+    case Medal.try_again:
+      print("Try again in sometime");
+  } */
+
