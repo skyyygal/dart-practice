8dbb9c7d3731d03c35d3dfb54b18823dd571260a
skyyygal <heeramnakp@gmail.com>
2025-11-30 00:36:34 +0530

    FIZZ BUZZ Problem

--- strings/finding_replacing_strings.dart
+++ strings/finding_replacing_strings.dart
@@ -1,9 +1,25 @@
 //finding and replacing the strings.
 
+// void main() {
+//   const text = 'I like pizza';
+//   const toppings = 'with tomatoes';
+//   const favorite = '$text $toppings';
+//   final newText = favorite.replaceAll('pizza', 'pasta');
+//   const newFavorite = 'Now i like curry';
+//   print(newFavorite);
+//   print(newText);
+
 void main() {
-  int time = 4;
-  String type = time > 7 ? 'order' : 'cook';
-  print(type);
-  String email = 'test@example.com';
-  print(email.isNotEmpty && email.contains('@'));
+  for (var i = 1; i <= 15; i++) {
+    if (i % 3 == 0 && i % 5 == 5) {
+      print("Fizz buzz");
+    } else if (i % 3 == 0) {
+      print("fizz");
+    } else if (i % 5 == 0) {
+      print('buzz');
+    } else {
+      print(i);
+    }
+  }
+  print("done");
 }
