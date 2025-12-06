530a3c8f41f7fe1812b97362c3eb48c430f98818
skyyygal <heeramnakp@gmail.com>
2025-12-07 04:03:15 +0530

    Folder structure

--- twoSumsVariations/variation4.dart
+++
@@ -1,8 +0,0 @@
-/* Variation 4 – Two Sum Less Than Target
-🔹 Find the maximum sum of two numbers less than target.
-Example:
-
-nums = [34, 23, 1, 24, 75, 33, 54, 8]
-target = 60
-Output = 58 // from 34 + 24
-💡 Key twist: Two pointers but track max sum instead of exact match. */
